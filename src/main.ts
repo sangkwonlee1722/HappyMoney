8df97294644aa7c47cf37b5f22cc42707e459521
JAEJIN <wowls9416@naver.com>
2024-01-16 21:56:15 +0900

    init:[frontsetting] openapi cors 권한 접근을 위한 프록시서버 추가

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
+import express, { Express, RequestHandler } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>("SERVER_PORT");
   app.setGlobalPrefix("api");
@@ -19,7 +22,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
-  /**스웨거 */
+
+  // Swagger 설정
   const config = new DocumentBuilder()
     .setTitle("HappyMoney")
     .setDescription("Node.js 3기 최종프로젝트 happymoney api")
@@ -35,9 +39,37 @@ async function bootstrap() {
     }
   });
 
-  // 정적파일 제공
+  // CORS 설정 및 프록시 설정
+  const corsProxy: Express = express();
+  const proxyHandler: RequestHandler = createProxyMiddleware({
+    target: "https://openapi.koreainvestment.com:9443",
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: { "^/": "/" },
+    onProxyRes: (proxyRes, req, res) => {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"; // 또는 실제 프론트엔드 호스트 주소
+      proxyRes.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS";
+      proxyRes.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept";
+    }
+  });
+
+  corsProxy.use("/", proxyHandler);
+
+  const nestApp = app.getHttpAdapter().getInstance();
+  const proxyServer = corsProxy.listen(8080, () => {
+    console.log("CORS Proxy Server is running on http://localhost:8080");
+  });
+
+  // 정적 파일 제공
   app.useStaticAssets(join(__dirname, "..", "assets"));
 
   await app.listen(port);
+
+  // 애플리케이션 종료 이벤트 대신 프록시 서버 종료를 기다림
+  process.on("SIGINT", () => {
+    proxyServer.close();
+    process.exit();
+  });
 }
+
 bootstrap();
