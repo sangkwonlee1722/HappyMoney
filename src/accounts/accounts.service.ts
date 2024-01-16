df544fe147045b26a2b8116c2e6ce06ec961a559
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-01-16 22:12:29 +0900

    Feat: [account/create] UserInfo 데코레이터 이용
    1) 로그인 시 Req 에서 유저 정보 활용
    - UserInfo 데코레이터에서 현재 로그인 된 유저의 정보를 확인
    - API 호출 시 유저 아이디를 가져옴
    
    2) 계좌 생성 시 계좌번호 추가
    - 유저가 이름 외의 공정된 계좌 구분 값을 확인할 필요가 있다고 판단

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { CreateAccountDto } from "./dto/create-account.dto";
 import { UpdateAccountDto } from "./dto/update-account.dto";
 import { v4 as uuidv4 } from "uuid";
@@ -14,16 +14,59 @@ export class AccountsService {
   ) {}
 
   async createNewAccount(name: string, userId: number): Promise<void> {
+    const myAccountsNumbers = await this.countMyAllAcounts(userId);
+
+    if (myAccountsNumbers >= 3) {
+      throw new BadRequestException("계좌는 계정 당 3개까지만 개설이 가능합니다.");
+    }
+
+    const accountNumber: string = await this.generateUniqueAccountNumber();
+
     await this.accountRepository.save({
       name,
-      userId
+      userId,
+      accountNumber
     });
+  }
+
+  /* 계정 당 3개까지 계좌 개설 제한 */
+  async countMyAllAcounts(userId: number): Promise<number> {
+    return await this.accountRepository.count({ where: { userId } });
+  }
+
+  /* 랜덤으로 계좌 번호 생성 */
+  async generateAccountNumber(): Promise<string> {
+    const min = 1000000000;
+    const max = 9999999999;
+    const randomNum: number = Math.floor(Math.random() * (max - min)) + min;
+
+    const formattedAccountNum: string = randomNum.toString().replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+    return formattedAccountNum;
+  }
+
+  /* 생성된 계좌번호가 DB에 있는지 확인 */
+  async findAccountByAccountNumber(accountNumber: string): Promise<Account> {
+    const account = await this.accountRepository.findOne({ where: { accountNumber } });
+
+    return account;
+  }
+
+  /* 생성된 계좌번호가 DB에 있을 경우 다시 생성하는 함수 */
+  async generateUniqueAccountNumber(): Promise<string> {
+    const accountNumber: string = await this.generateAccountNumber();
+    const existAccount: Account = await this.findAccountByAccountNumber(accountNumber);
+
+    if (existAccount) {
+      return this.generateUniqueAccountNumber(); // 재귀 호출로 다시 계좌번호 생성
+    }
+
+    return accountNumber;
   }
 
   async findAllMyAccountsById(userId: number): Promise<Account[]> {
     const accounts: Account[] = await this.accountRepository.find({
       where: { userId },
-      select: ["id", "name", "point", "userId"] // 주식 총 평가금액 추가 예정
+      select: ["id", "name", "point", "userId", "accountNumber"] // 주식 총 평가금액 추가 예정
     });
 
     return accounts;
@@ -34,7 +77,7 @@ export class AccountsService {
       .createQueryBuilder("a")
       .where("a.userId=:userId", { userId })
       .andWhere("a.id=:accountId", { accountId })
-      .select(["a.id", "a.name", "a.point", "a.userId"]) // 계좌가 보유한 주식 목록 조인 예정
+      .select(["a.id", "a.name", "a.point", "a.userId", "a.accountNumber"]) // 계좌가 보유한 주식 목록 조인 예정
       .getOne();
 
     if (!account) {
