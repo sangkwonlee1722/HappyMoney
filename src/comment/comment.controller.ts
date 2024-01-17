aa020d85ef8b650a4e31b607a0af759029771967
sangkwonlee1722 <kjess8335@gmail.com>
2024-01-17 21:47:35 +0900

    Comment:[comment/create] 엔티티 데코레이터, 컨트롤러 주석

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -10,26 +10,41 @@ import { ApiTags } from "@nestjs/swagger";
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  /**
+   * 댓글 작성
+   * @param createCommentDto
+   * @returns
+   */
   @Post()
   async create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
+  /**
+   * 댓글 전체 조회
+   * @returns
+   */
   @Get()
   async findAll() {
     return this.commentService.findAll();
   }
 
-  @Get(":id")
-  async findOne(@Param("id") id: number) {
-    return this.commentService.findOne(id);
-  }
-
+  /**
+   * 댓글 수정
+   * @param id
+   * @param updateCommentDto
+   * @returns
+   */
   @Patch(":id")
   async update(@Param("id") id: number, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentService.update(id, updateCommentDto);
   }
 
+  /**
+   * 댓글 삭제
+   * @param id
+   * @returns
+   */
   @Delete(":id")
   async remove(@Param("id") id: number) {
     return this.commentService.remove(id);
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,9 +1,12 @@
 import { Entity, Column, ManyToOne } from "typeorm";
 import { User } from "src/user/entities/user.entity";
 import { BaseEntity } from "src/common/entities/base.entity";
+import { IsNotEmpty, IsString } from "class-validator";
 
 @Entity()
 export class Comment extends BaseEntity {
+  @IsNotEmpty({ message: "댓글을 입력해 주세요." })
+  @IsString()
   @Column()
   content: string;
 
