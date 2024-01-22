295c92e613be953d5f43cbdbd8fb6843f182466a
sangkwonlee1722 <kjess8335@gmail.com>
2024-01-22 21:41:39 +0900

    Feat:[noticemain/front]공지사항 메인페이지

---
+++ assets/js/notice-main.js
@@ -0,0 +1,37 @@
+const getNoticeData = async () => {
+  try {
+    const apiUrl = `/api/notices`;
+
+    const result = await axios.get(apiUrl);
+    const latestNotices = result.data.data.slice(0, 10); // 최신 10개
+
+    const mainDom = document.querySelector(".notice-list");
+
+    mainDom.innerHTML = latestNotices
+      .map((notice) => {
+        const { title, createdAt } = notice;
+
+        const formattedDate = createdAt.split("T")[0];
+
+        return `
+        <li class="contents">
+          <a href="#none"></a>
+          <div class="list-info">
+            <div class="classification">NOTICE</div>
+            <div class="title">${title}</div>
+          </div>
+          <div class="list-info-2">
+            <div class="author">관리자</div>
+            <div class="date">${formattedDate}</div>
+          </div>
+        </li>
+        <hr />
+        `;
+      })
+      .join("");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+await getNoticeData();
