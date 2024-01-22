98e963298855a372bfc6168ae65bc3c8bb01690f
JAEJIN <wowls9416@naver.com>
2024-01-22 20:04:16 +0900

    feat:[twit/create] 쪽지API 작업 완료 및 프론트 팝업 예시 작업

---
+++ src/twit/twit.service.ts
@@ -0,0 +1,84 @@
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { CreateTwitDto } from "./dto/create-twit.dto";
+import { User } from "src/user/entities/user.entity";
+import { Twit } from "./entities/twit.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class TwitService {
+  constructor(
+    @InjectRepository(Twit)
+    private readonly twitRepository: Repository<Twit>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async sendTwit({ id }: User, { contents, receiveNickname }: CreateTwitDto) {
+    const receiver = await this.findUserByNickname(receiveNickname);
+
+    if (receiver.id === id) throw new BadRequestException({ success: false, message: "나에게 보낼 수 없습니다." });
+    if (!receiver) throw new NotFoundException({ success: false, message: "수신자를 찾을 수 없습니다." });
+    console.log(receiver);
+
+    const sendTwit = await this.twitRepository.save({
+      senderId: id,
+      receiveId: receiver.id,
+      contents
+    });
+
+    console.log(sendTwit);
+    return sendTwit;
+  }
+
+  async getSendTwit({ id }: User) {
+    const getSends = await this.twitRepository.find({ where: { senderId: id } });
+
+    if (getSends.length === 0) return { message: "보낸 쪽지가 없습니다." };
+
+    return getSends;
+  }
+
+  async getReceiveTwit({ id }: User) {
+    const getReceives = await this.twitRepository.find({ where: { receiveId: id } });
+
+    if (getReceives.length === 0) return { message: "받은 쪽지가 없습니다." };
+
+    return getReceives;
+  }
+
+  async getTwitDetail(id: number, user: User) {
+    await this.twitRepository.update({ id, receiveId: user.id }, { isRead: true });
+
+    const getTwitDetail = await this.twitRepository.findOne({ where: { id } });
+
+    if (!getTwitDetail) throw new NotFoundException({ success: false, message: "해당 쪽지를 찾을 수 없습니다." });
+
+    return getTwitDetail;
+  }
+
+  async deleteSendTwit(id: number, user: User) {
+    await this.twitRepository.update({ id, senderId: user.id }, { isDeletedBySender: true });
+
+    const deleteSendTwit = await this.twitRepository.findOne({ where: { id, senderId: user.id } });
+
+    if (!deleteSendTwit) throw new BadRequestException({ success: false, message: "삭제할 수 없습니다." });
+
+    return deleteSendTwit;
+  }
+
+  async deleteReceiveTwit(id: number, user: User) {
+    await this.twitRepository.update({ id, receiveId: user.id }, { isDeletedByReceiver: true });
+
+    const deleteReceiveTwit = await this.twitRepository.findOne({ where: { id, receiveId: user.id } });
+
+    if (!deleteReceiveTwit) throw new BadRequestException({ success: false, message: "삭제할 수 없습니다." });
+
+    return deleteReceiveTwit;
+  }
+
+  // 닉네임으로 해당 아이디 받아오기
+  async findUserByNickname(nickname: string): Promise<User | undefined> {
+    return this.userRepository.createQueryBuilder("user").where("user.nickName = :nickname", { nickname }).getOne();
+  }
+}
