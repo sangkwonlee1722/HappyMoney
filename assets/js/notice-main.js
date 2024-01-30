996e1e76065869018d7ff592cebf9756596936c2
sangkwonlee1722 <kjess8335@gmail.com>
2024-01-30 10:17:54 +0900

    Feat:[notice/front] 공지사항 수정, 삭제 구현
    - 공지사항 삭제
    - 삭제 후 전체조회 페이지로
    - 글쓰기 후 전체조회 페이지로
    - 상세페이지 목록으로 버튼 수정
    - 상세페이지에서 수정누르면 수정페이지로
    - 수정 기능 구현
    - 수정 완료 후 다시 해당 상세페이지로

--- assets/js/notice-main.js
+++ assets/js/notice-main.js
@@ -1,16 +1,15 @@
 import { baseUrl } from "./common.js";
 import renderPagination from "/js/pagenation.js";
+import getToken from "./common.js";
 
 const params = new URLSearchParams(window.location.search);
 const noticePage = params.get("page");
-console.log(noticePage);
 const getNoticeData = async () => {
   try {
     const apiUrl = baseUrl + `notices?page=${noticePage}`;
     const result = await axios.get(apiUrl);
 
     const noticeList = result.data.list;
-    console.log(noticeList);
     const noticeTotal = result.data.total;
     return { noticeList, noticeTotal };
   } catch (error) {
@@ -30,7 +29,7 @@ const getNoticeData = async () => {
 
         return `
         <li class="contents">
-        <a href="http://localhost:3000/views/notice-page.html#${id}"></a>
+        <a href="http://localhost:3000/views/notice-page.html?id=${id}"></a>
           <div class="list-info">
             <div class="classification">NOTICE</div>
             <div class="title">${title}</div>
@@ -50,7 +49,11 @@ const getNoticeData = async () => {
   }
 })();
 
-const writeButton = document.querySelector(".hm-button a");
-writeButton.addEventListener("click", () => {
-  window.location.href = "http://localhost:3000/views/notice-posting.html#none";
-});
+const writeButton = document.querySelector(".hm-button.hm-gray-color");
+if (writeButton) {
+  writeButton.addEventListener("click", () => {
+    window.location.href = "http://localhost:3000/views/notice-posting.html";
+  });
+} else {
+  console.error("Write button not found.");
+}
