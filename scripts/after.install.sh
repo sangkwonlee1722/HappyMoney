2c6a320bdf766297dbc4d8746ef74189e8a0ef23
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-02-09 00:51:09 +0900

    Fix: [test/cd] CD 테스트 3차
    - CodeDeploy에서 main.js가 이미 존재한다고 나옴
    - pm2로 서버를 실행중에 있어서 발생하는 문제로 보여서 수정

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -1,10 +1,9 @@
 !/bin/bash
-sudo chmod -R 777 /home/ubuntu/HappyMoney // sudo 권한 부여
+sudo chmod -R 777 /home/ubuntu/HappyMoney
 
 #navigate into our working directory
-cd /home/ubuntu/HappyMoney 프로젝트 루트 경로 접근
+cd /home/ubuntu/HappyMoney
 
-#install node modules & update swagger & pm2 reload
 sudo npm ci
 sudo npm run build
 sudo pm2 reload api-server
--- scripts/before.install.sh
+++ scripts/before.install.sh
@@ -1,8 +1,6 @@
 !/bin/bash
-sudo chmod -R 777 /home/ubuntu/HappyMoney // sudo 권한 부여
+sudo chmod -R 777 /home/ubuntu/HappyMoney
 
 #navigate into our working directory
-cd /home/ubuntu/HappyMoney 프로젝트 루트 경로 접근
-
-#install node modules & update swagger & pm2 reload
+cd /home/ubuntu/HappyMoney
 sudo pm2 stop api-server
