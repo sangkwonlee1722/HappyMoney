966c178faa2791e18949514f705ab9838c4e15a5
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-02-08 23:50:30 +0900

    Feat: [test/cd] CI/CD 테스트

---
+++ scripts/after.install.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+sudo chmod -R 777 /home/ubuntu/HappyMoney // sudo 권한 부여
+
+#navigate into our working directory
+cd /home/ubuntu/HappyMoney 프로젝트 루트 경로 접근
+
+#install node modules & update swagger & pm2 reload
+sudo npm ci
+sudo npm run build
+sudo pm2 reload api-server
