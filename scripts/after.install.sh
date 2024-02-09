a5fe71c211bab293fc4780beeed23fbe34e8e564
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-02-09 21:55:17 +0900

    Fix: [test/cd] sh 스크립트 수정

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/HappyMoney
 
-npm ci
-npm run build
-pm2 start dist/main.js --name api-server
+/usr/local/bin/npm ci
+/usr/local/bin/npm run build
+/usr/local/bin/pm2 start dist/main.js --name api-server
