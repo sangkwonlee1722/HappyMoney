00bd3065a0b9f54262bd3050deac5aa6b0e421f5
Geniusjun6 <146689742+Geniusjun6@users.noreply.github.com>
2024-02-09 21:27:19 +0900

    Fix: [test/cd] sh 명령어 수정

--- scripts/after.install.sh
+++ scripts/after.install.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/HappyMoney
 
 npm ci
 npm run build
-pm2 reload api-server
+pm2 start dist/main.js --name api-server
--- scripts/before.install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/HappyMoney
-pm2 stop api-server
