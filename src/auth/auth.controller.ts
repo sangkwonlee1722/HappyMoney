a1908170aeb8a2285b4b9ea990b3299f261120bc
minjae Kim <cmg981548@gmail.com>
2024-02-19 15:43:35 +0900

    Fix: [user/prontend] auth.controller 스웨거 , search.controller 스웨거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,9 +20,19 @@ export class AuthController {
   ) {}
 
   //-----------------------구글 로그인-----------------------------//
+
+  /**
+   * 구글 소셜 로그인
+   * @returns
+   */
   @Get("google/login")
   @UseGuards(AuthGuard("google"))
   async loginGoogle(@Req() req: Request & IOAuthUser, @Res() res: Response) {}
+  // 정상적으로 로그인이 되면 COOKIE에 TOKEN을 저장하며 로그인을 하게된다.
+  /**
+   * 구글 로그인 리다이렉트 URI
+   * @returns
+   */
   @Get("google/callback")
   @UseGuards(AuthGuard("google"))
   async googleLoginCallback(@Req() req: any, @Res() res: any) {
@@ -32,13 +42,21 @@ export class AuthController {
   }
 
   //-----------------------카카오 로그인-----------------------------//
+  /**
+   * 카카오 소셜 로그인
+   * @returns
+   */
   @Get("kakao/login")
   @UseGuards(AuthGuard("kakao"))
   async loginKakao(
     @Req() req: Request & IOAuthUser, //
     @Res() res: Response
   ) {}
-
+  // 정상적으로 로그인이 되면 COOKIE에 TOKEN을 저장하며 로그인을 하게된다.
+  /**
+   * 카카오 로그인 리다이렉트 URI
+   * @returns
+   */
   @Get("kakao/callback")
   @UseGuards(AuthGuard("kakao"))
   async kakaoLoginCallback(@Req() req: any, @Res() res: any) {
@@ -48,13 +66,21 @@ export class AuthController {
   }
 
   //-----------------------네이버 로그인-----------------------------//
+  /**
+   * 네이버 소셜 로그인
+   * @returns
+   */
   @Get("naver/login")
   @UseGuards(AuthGuard("naver"))
   async loginNaver(
     @Req() req: Request & IOAuthUser, //
     @Res() res: Response
   ) {}
-
+  // 정상적으로 로그인이 되면 COOKIE에 TOKEN을 저장하며 로그인을 하게된다.
+  /**
+   * 네이버 로그인 리다이렉트 URI
+   * @returns
+   */
   @Get("naver/callback")
   @UseGuards(AuthGuard("naver"))
   async naverLoginCallback(@Req() req: any, @Res() res: any) {
@@ -63,6 +89,10 @@ export class AuthController {
     return res.redirect("/views/main.html");
   }
 
+  /**
+   * 서버 health 체크
+   * @returns
+   */
   @Get("health")
   async healthCheck() {
     return {
